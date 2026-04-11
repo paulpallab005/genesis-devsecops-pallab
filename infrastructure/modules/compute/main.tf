c0ee3c87f8f563ba5209e5fb850ee51f36b51822
Pallab Paul <paulpallab005@gmail.com>
2026-04-11 17:03:09 +0530

    Remove unsupported Lambda permission with condition block - authorization_type=NONE should allow public access

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -108,14 +108,6 @@ resource "aws_lambda_function_url" "api" {
   }
 }
 
-# Resource-based policy to allow public invocation (required even with authorization_type = "NONE")
-resource "aws_lambda_permission" "function_url_public" {
-  statement_id           = "AllowPublicFunctionUrl"
-  action                 = "lambda:InvokeFunctionUrl"
-  function_name          = aws_lambda_function.api.function_name
-  principal              = "*"
-}
-
 # CloudWatch Log Group for Lambda (explicit creation for control)
 resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${aws_lambda_function.api.function_name}"
