669a75293cd8a8ff2142e0bf0185f781c0eccfb0
Pallab Paul <paulpallab005@gmail.com>
2026-04-11 15:51:28 +0530

    Simplify ECR image tagging to single unique tag

--- infrastructure/modules/compute/main.tf
+++ infrastructure/modules/compute/main.tf
@@ -125,34 +125,3 @@ resource "aws_cloudwatch_log_group" "lambda" {
     Owner       = var.owner
   }
 }
-
-# Outputs
-output "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.app.repository_url
-}
-
-output "ecr_repository_name" {
-  description = "Name of the ECR repository"
-  value       = aws_ecr_repository.app.name
-}
-
-output "lambda_function_arn" {
-  description = "ARN of the Lambda function"
-  value       = aws_lambda_function.api.arn
-}
-
-output "lambda_function_name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.api.function_name
-}
-
-output "lambda_function_url" {
-  description = "HTTP URL for the Lambda function"
-  value       = aws_lambda_function_url.api.function_url
-}
-
-output "lambda_log_group_name" {
-  description = "Name of the CloudWatch log group"
-  value       = aws_cloudwatch_log_group.lambda.name
-}
--- infrastructure/modules/observability/main.tf
+++ infrastructure/modules/observability/main.tf
@@ -354,28 +354,3 @@ resource "aws_cloudwatch_metric_alarm" "duration_anomaly" {
   }
 }
 
-# Outputs
-output "dashboard_name" {
-  description = "Name of the CloudWatch dashboard"
-  value       = aws_cloudwatch_dashboard.api.dashboard_name
-}
-
-output "dashboard_arn" {
-  description = "ARN of the CloudWatch dashboard"
-  value       = aws_cloudwatch_dashboard.api.dashboard_arn
-}
-
-output "sns_topic_arn" {
-  description = "ARN of the SNS topic for alarms"
-  value       = aws_sns_topic.alarms.arn
-}
-
-output "error_rate_alarm_name" {
-  description = "Name of the error rate alarm"
-  value       = aws_cloudwatch_metric_alarm.error_rate.alarm_name
-}
-
-output "duration_alarm_name" {
-  description = "Name of the duration anomaly alarm"
-  value       = aws_cloudwatch_metric_alarm.duration_anomaly.alarm_name
-}
